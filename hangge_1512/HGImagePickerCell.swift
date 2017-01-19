c0f980d598dd0b47a7b889137dc1d725ed915fc9
Wyz <Wyz@Wangyazhuo-MacBookPro.local>
2017-01-19 10:23:54 +0800

    一个图片选择器，swift版本可多选

---
+++ hangge_1512/HGImagePickerCell.swift
@@ -0,0 +1,26 @@
+//
+//  HGImagePickerCell.swift
+//  Wyz
+//
+//  Created by Wyz on 2017/1/7.
+//  Copyright © 2017年 Wyz. All rights reserved.
+//
+
+import UIKit
+
+//相簿列表单元格
+class HGImagePickerCell: UITableViewCell {
+    //相簿名称标签
+    @IBOutlet weak var titleLabel:UILabel!
+    //照片数量标签
+    @IBOutlet weak var countLabel:UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layoutMargins = UIEdgeInsets.zero
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
---
+++ hangge_1512/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Wyz
+//
+//  Created by Wyz on 2017/1/7.
+//  Copyright © 2017年 Wyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //“开始选择照片”按钮点击
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        //开始选择照片，最多允许选择4张
+        _=self.presentHGImagePicker(maxSelected: 3, completeHandler: { (assets) in
+            //结果处理
+            print("共选择了\(assets.count)张图片，分别如下：")
+            for asset in assets {
+                print(asset)
+            }
+
+        })
+//        _ = self.presentHGImagePicker(maxSelected:4) { (assets) in
+//            //结果处理
+//            print("共选择了\(assets.count)张图片，分别如下：")
+//            for asset in assets {
+//                print(asset)
+//            }
+//        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
